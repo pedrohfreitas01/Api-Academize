c3e520a43b81a60295348fac440f11916dd86624
pedrohfc <pedrofreitas_ph@hotmail.com>
2025-08-05 10:11:52 -0300

    📦 chore: inicia o projeto com estrutura base e configuração do ambiente Node.js

---
+++ src/env/index.ts
@@ -0,0 +1,18 @@
+//carregar as env e validar
+
+import "dotenv/config";
+import z from "zod";
+
+const envSchema = z.object({
+  NODE_ENV: z.enum(["dev", "test", "production"]).default("dev"),
+  PORT: z.coerce.number().default(3333),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (_env.success === false) {
+  console.error("❌ Invalid environment variables", z.treeifyError(_env.error));
+  throw new Error("Invalid environment variables");
+}
+
+export const env = _env.data;
