ed14b354c60d85dd901ff918a45a4210f27282ae
pedrohfc <pedrofreitas_ph@hotmail.com>
2025-08-26 10:48:54 -0300

    ✨ feat(auth): implementa caso de uso de autenticação com testes e controller

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,9 +1,9 @@
 import type { FastifyInstance } from "fastify";
 import { register } from "./controllers/register.js";
+import { authenticate } from "./controllers/authenticate.js";
 
+export async function appRoutes(app: FastifyInstance) {
+  app.post("/users", register);
 
-
-export async function appRoutes(app:FastifyInstance) {
-    app.post("/users", register);
-
+  app.post("/sessions", authenticate);
 }
