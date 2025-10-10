c97b5a45c0e1ac25e44351a6b7e23b1b9c7335ab
pedrohfc <pedrofreitas_ph@hotmail.com>
2025-10-10 07:22:10 -0300

    tdd & mocking

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -5,6 +5,17 @@ import type { CheckInsRepository } from "repositories/check-ins-repository.js";
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = [];
 
+  async findByUserIdOnDate(userId: string, date: Date) {
+    const checkInOnSameDate = this.items.find(
+      (checkIn) => checkIn.userId === userId
+    );
+
+    if (!checkInOnSameDate) {
+      return null;
+    }
+    return checkInOnSameDate;
+  }
+
   async create(data: Prisma.CheckInUncheckedCreateInput) {
     const checkIn = {
       id: randomUUID(),
